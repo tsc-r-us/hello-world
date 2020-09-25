562666731cc6a841be4ccd0b7280464e2a054b58
Eric Dowell <github@ericdowell.com>
2020-09-25 08:52:27 -0500

    Adding watch script.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ export class Hello {
     world() {
         console.log(`${this.constructor.name} World!`)
     }
+
+    there() {
+        console.log(`${this.constructor.name} there!`);
+    }
 }
 
 const hello = new Hello()
 
 hello.world()
+hello.there();
